f98d9ba95657f3f53f11dc556bcbfee935b6ba20
oxbown <oxbown91@gmail.com>
2020-03-30 22:26:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
 wget https://github.com/oxbown/NornsMother/releases/latest/download/NornsMother.deb && sudo apt install -y ./NornsMother.deb && sudo rm ./NornsMother.deb
+cp /usr/local/sidekick/sidekick.json ~/sidekick
