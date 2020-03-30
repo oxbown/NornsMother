e46b34ce99c9ca3a18f1ace58e11289a95940c84
oxbown <oxbown91@gmail.com>
2020-03-30 20:35:08 +0200

    Fixed start_pd.sh

--- NornsMother/home/we/sidekick/patches/Organelle/start_pd.sh
+++ NornsMother/home/we/sidekick/patches/Organelle/start_pd.sh
@@ -1,4 +1,5 @@
 #/bin/bash
-find ./ -name versioncheck.pd -exec cp ./mother/versioncheck.pd {} \;
+
+find ./ -name versioncheck.pd -exec cp /home/we/sidekick/patches/Organelle/mother/versioncheck.pd {} \;
 
 sudo systemctl start amidiauto
